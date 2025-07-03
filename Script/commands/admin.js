7ba1d60e0c350b776f1c2636a4c2a57deb8d906e
daduvai00 <tinverkhan0@gmail.com>
2025-07-03 15:50:20 +0600

    Update admin.js

--- Script/commands/admin.js
+++ Script/commands/admin.js
@@ -1,5 +1,4 @@
 const axios = require("axios");
-const request = require("request");
 const fs = require("fs-extra");
 const moment = require("moment-timezone");
 
@@ -17,28 +16,24 @@ module.exports.config = {
 module.exports.run = async function({ api, event }) {
     var time = moment().tz("Asia/Dhaka").format("DD/MM/YYYY hh:mm:ss A");
 
-    var callback = () => api.sendMessage({
+    // Send the message without an image
+    return api.sendMessage({
         body: `
 ┏━━━━━━━━━━━━━━━━━━━━━┓
 ┃    🌟 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢 🌟      
 ┣━━━━━━━━━━━━━━━━━━━━━┫
-┃ 👤 𝐍𝐚𝐦𝐞    : TâMïM Khan  ッ
-┃ 🚹 𝐆𝐞𝐧𝐝𝐞𝐫   : 𝐌𝐚𝐥𝐞
-┃ ❤️ 𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧  : Single 
-┃ 🎂 𝐀𝐠𝐞      : 21
+┃ 👤 𝐍𝐚𝐦𝐞   : Tâmïm Khan ッ
+┃ 🚹 𝐆𝐞𝐧𝐝𝐞𝐫  : 𝐌𝐚𝐥𝐞
+┃ ❤️ 𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧 : Singel
+┃ 🎂 𝐀𝐠𝐞     : 21
 ┃ 🕌 𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧  : 𝐈𝐬𝐥𝐚𝐦
 ┃ 🏫 𝐄𝐝𝐮𝐜𝐚𝐭𝐢𝐨𝐧 : BBA
 ┃ 🏡 𝐀𝐝𝐝𝐫𝐞𝐬𝐬  : Khulna, 𝐁𝐚𝐧𝐠𝐥𝐚𝐝𝐞𝐬𝐡
 ┣━━━━━━━━━━━━━━━━━━━━━┫
-,,,,,,,,Assistant AI,,,,,,,
+,,,,,,,,,,,,AI Assistant,,,,,,,,,,,, 
 ┣━━━━━━━━━━━━━━━━━━━━━┫
 ┃ 🕒 𝐔𝐩𝐝𝐚𝐭𝐞𝐝 𝐓𝐢𝐦𝐞:  ${time}
 ┗━━━━━━━━━━━━━━━━━━━━━┛
-        `,
-        attachment: fs.createReadStream(__dirname + "/cache/1.png")
-    }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.png"));
-  
-    return request(encodeURI(`https://graph.facebook.com/100000478146113/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`))
-        .pipe(fs.createWriteStream(__dirname + '/cache/1.png'))
-        .on('close', () => callback());
+    `,
+    }, event.threadID);
 };
